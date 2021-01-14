beb2d5e943140f4a5cb2ebcf2642edcc7f3168c0
Nicholas Kniola <nickkniola@me.com>
2021-01-14 00:00:23 +0000

    Add login form for trying out the web app

--- client/pages/Main.jsx
+++ client/pages/Main.jsx
@@ -12,13 +12,32 @@ export default class Main extends React.Component {
 
   render() {
     return (
-      <div className="main-container">
-        <div className="text-container">
-          <h1 className="header-main"><img className="header-logo" src="images/hang_logo.png"></img>Hang</h1>
-          <p className="subtitle-main">The only web app which makes plans for you!</p>
-        </div>
-        <div className="button-container">
-          <button className="ui primary button sign-up-button" type="button" onClick={this.handleClick}>Sign Up</button>
+      <div className="main-container ui middle aligned center aligned grid">
+        <div className="column">
+          <div className="text-container">
+            <h1 className="header-main"><img className="header-logo" src="images/hang_logo.png"></img>Hang</h1>
+            <p className="subtitle-main">The only web app which makes plans for you!</p>
+          </div>
+          <form className="ui large form login-form">
+            <div className="ui segment">
+              <div className="field">
+                <div className="ui left icon input">
+                  <i className="user icon"></i>
+                  <input type="text" name="email" placeholder="E-mail address" />
+                </div>
+              </div>
+              <div className="field">
+                <div className="ui left icon input">
+                  <i className="lock icon"></i>
+                  <input type="password" name="password" placeholder="Password" />
+                </div>
+              </div>
+              <div className="ui fluid large primary submit button">Try It Out</div>
+            </div>
+          </form>
+          <div className="button-container">
+            <button className="ui primary button sign-up-button" type="button" onClick={this.handleClick}>Sign Up</button>
+          </div>
         </div>
       </div>
     );

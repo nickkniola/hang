9690906ee3addbc3dcd6b3a19f783870d9c5957b
Nicholas Kniola <nickkniola@me.com>
2021-01-15 01:54:12 +0000

    Add next to middleware

--- server/index.js
+++ server/index.js
@@ -76,7 +76,7 @@ app.post('/api/auth/sign-in', (req, res, next) => {
     .catch(err => next(err));
 });
 
-app.use((req, res) => {
+app.use((req, res, next) => {
   res.sendFile('/index.html', {
     root: path.join(__dirname, 'public')
   });
